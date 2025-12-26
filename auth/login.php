7d65255a05a6248502da41190b299f6c006a2cf4
Mahmoud Allam <malllam146@gmail.com>
2025-12-26 13:19:27 +0200

    firs commit

---
+++ auth/login.php
@@ -0,0 +1,19 @@
+<?php
+require_once '../includes/config.php';
+require_once '../includes/auth.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  // check db
+  // set session
+  header("Location: /profile/");
+}
+?>
+
+<form method="POST">
+  <input name="email">
+  <input type="password" name="password">
+  <button>Login</button>
+</form>
