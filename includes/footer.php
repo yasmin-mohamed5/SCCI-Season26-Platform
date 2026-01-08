f92c7e276ad03f8c6dbac8b6cfc464d06cb34719
omarRaslan66 <omarraslan660@gmail.com>
2026-01-09 00:03:02 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,6 @@
 
 <head>
   <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <!-- google font -->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -33,19 +32,29 @@
   <footer>
     <div class="footerCont">
       <div class="footerLeft footerWide">
-        <img src="../assets/icons/logoSCCI.png" alt="" />
+        <img loading="lazy" src="../assets/icons/logoSCCI.png" alt="SCCI img"/>
       </div>
 
       <div class="contactForm footerForm">
         <h3>Quick Links</h3>
         <section>
-          <a href="home.html">home</a>
-          <a href="about us.html">about us</a>
-          <a href="gallery.html">gallery</a>
-          <a href="workshops.html">workshops</a>
-          <a href="crew.html">crew</a>
-          <a href="login.html">login</a>
-          <a href="profile.html">profile</a>
+          <a href="../home.php">home</a>
+          <a href="../about.php">about us</a>
+          <a href="../gallery.php">gallery</a>
+          <a href="../workshops.php">workshops</a>
+          <a href="../crew.php">crew</a>
+          <a href="../profile.php">profile</a>
+          <a href="../auth/login.php">login</a>
+        </section>
+      </div>
+
+      <div class="contactForm footerForm">
+        <h3>Our Committees</h3>
+        <section>
+          <a href="../workshopsDetails.php">devolgy</a>
+          <a href="../workshopsDetails.php">market</a>
+          <a href="../workshopsDetails.php">data station</a>
+          <a href="../workshopsDetails.php">tech solve</a>
         </section>
       </div>
 
@@ -53,9 +62,9 @@
         <h3>social media</h3>
 
         <div class="footerSocial">
-          <a target="_blank" href="https://www.facebook.com/scci.cu"><i class="fa-brands fa-facebook"></i> <span class="socialText"> facebook page</span></a>
-          <a target="_blank" href="https://www.instagram.com/scci.cu/"><i class="fa-brands fa-instagram"></i> <span class="socialText"> instagram page</span></a>
-          <a target="_blank" href="https://www.linkedin.com/in/scci-cu-478a09390/"><i class="fa-brands fa-linkedin"></i> <span class="socialText"> linkedin page</span></a>
+          <a target="_blank" href="https://www.facebook.com/scci.cu"><i class="fa-brands fa-facebook"></i> <span class="socialText"> facebook</span></a>
+          <a target="_blank" href="https://www.instagram.com/scci.cu/"><i class="fa-brands fa-instagram"></i> <span class="socialText"> instagram</span></a>
+          <a target="_blank" href="https://www.linkedin.com/in/scci-cu-478a09390/"><i class="fa-brands fa-linkedin"></i> <span class="socialText"> linkedin</span></a>
           <a target="_blank" href=""><i class="fa-solid fa-envelope"></i> <span class="socialText"> scci2026@gmail.com</span></a>
         </div>
       </div>
@@ -64,12 +73,12 @@
     <div class="copy">
       <!-- <hr class="footer-line"> -->
       <p>
-        Copyright SCCI-2026. Privileged and confidential. All rights reserved.
+        SCCI - Shaping the Arc of Learning
       </p>
     </div>
   </footer>
 
-  <script src="../assets/js/all.min.js"></script>
+  <script src="../assets/js/all.min.js" defer></script>
 </body>
 
 </html>
