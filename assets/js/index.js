0f4d8c287a514801e54195160cb889914c0e22fd
AwadCoding <112626336+AwadCoding@users.noreply.github.com>
2026-01-09 03:56:12 +0200

    Implement side navbar toggle functionality
    Added event listeners to open and close the side navbar.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,26 @@
 
+
+// ===============================================navBar===============================================
+// variables
+const body = document.querySelector("body");
+const sideBtn = document.querySelector(".sideBtn");
+const sideBg = document.querySelector(".sideNav");
+const x = document.querySelector(".closeNav");
+
+// open side navbar
+sideBtn.addEventListener("click", () => {
+  body.classList.add("no-scrolling");
+  sideBg.classList.add("active");
+});
+
+// close side navbar
+x.addEventListener("click", () => {
+  body.classList.remove("no-scrolling");
+  sideBg.classList.remove("active");
+});
+
+sideBtn.addEventListener("click", () => {
+  console.log("clicked");
+
+});
+
