c664542cb6954d4556d876c00c6166c7a2eac4bf
yasmin-mohamed5 <mony.mony005@gmail.com>
2026-01-11 15:48:08 +0200

    add member panel files

---
+++ memberWorkshopPanel.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=Irish+Grover&display=swap"
+        rel="stylesheet">
+
+    <!-- Icons -->
+    <link rel="icon" href="assets/icons/logoSCCI.png" type="image/x-icon">
+    <!-- Font Awesome (Standard CDN) -->
+    <link rel="stylesheet" href="assets/css/all.min.css" />
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="assets/css/root.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="assets/css/navbar.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="assets/css/memberWorkshopPanel.css?v=<?php echo time(); ?>">
+    <!-- Custom Page Styles -->
+
+    <title>SCCI-Panel</title>
+</head>
+<body>
+    
+
+
+    <script src="assets/js/memberWorkshopPanel.js" defer></script>
+</body>
+</html>
