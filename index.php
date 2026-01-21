12967220cb53e7f74b435daa5cfc55a348039cab
imabdelrahman <itsabdelrahman.r@gmail.com>
2026-01-22 00:27:37 +0200

    Add HTML structure for landing page

--- index.php
+++ index.php
@@ -1,1 +1,42 @@
 
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seek The Peak</title>
+    <link rel="icon" href="./assets/icons/logoSCCI.png" type="image/png">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Stencil&display=swap" rel="stylesheet">
+
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="./assets/css/index.css">
+</head>
+
+<body>
+
+    <main>
+        <section class="landingHero" aria-label="Landing Hero Section">
+            <div class="overlay"></div>
+
+            <header class="landingContent">
+                <figure>
+                    <img src="./assets/icons/logoSCCI.png" class="landingLogo" alt="SCCI Logo">
+                </figure>
+
+                <h1>SEEK THE PEAK</h1>
+                <p>THE JOURNY THAT SHAPES YOUR FUTURE</p>
+            </header>
+        </section>
+    </main>
+<script src="./assets/js/indexRedirect.js"></script>
+</body>
+
+</html>
