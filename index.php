fb878526114a152b92eae9404b7b488b1c867985
HZOOMAAA <Hazem.Mohamed.22230228@commerce.helwan.edu.eg>
2026-01-25 01:42:56 +0200

    removed php

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<?php
-// عرض الأخطاء
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
-
-// توصيل بقاعدة البيانات
-include 'config.php';
-?>
 <!DOCTYPE html>
 <html lang="en">
 
